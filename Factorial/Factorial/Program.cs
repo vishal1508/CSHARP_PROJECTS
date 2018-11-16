ddcb1ed656fa5c9b42277423eeb5a702713fbc0b
GOVINDHARAJU PALANI <apgovinda@gmail.com>
2018-11-16 22:39:57 +0530

    Factorial added

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Calculator
+    {
+        public static long FactorialOf(int n)
+        {
+            int fact = 1;
+
+            while (n > 0)
+            {
+                fact = fact * n;
+                n--;
+            }
+
+            return fact;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Factorial of 5 = " + Calculator.FactorialOf(5));
+        }
+    }
+}
